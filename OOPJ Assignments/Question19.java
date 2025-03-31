265b050401e2a0ddb938551f3dfe943f5a44e418
gauravrthd <gauravrth012@gmail.com>
2025-03-31 23:46:57 +0530

    Delete OOPJ Assignments directory

--- OOPJ Assignments/Question19.java
+++
@@ -1,9 +0,0 @@
-//use ternaory operator cheak character is uppercase ,lowercase and not a latter
-class Question19{
-	public static void main(String[] args){
-		char ch = 'G';
-		String result = (ch >= 'A' && ch <= 'Z')? "Uppercase" : (ch >= 'a' && ch <= 'z')? "Lowercase":"Not Character";
-		System.out.print("Character :"+ ch + "is :" + result);
-	}
-
-}
--- OOPJ Assignments/Question20.java
+++
@@ -1,8 +0,0 @@
-// Retuen absulate value of given number by using ternary operator
-class Question20{
-	public static void main(String[] args){
-		int a = -99;
-		int result = (a<0)?-a:a;
-		System.out.print("Absulate value : "+result);
-	}
-}
